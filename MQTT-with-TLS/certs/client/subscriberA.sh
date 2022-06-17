44978a2b655f689ae02e90c34c8485b1fde13851
rodrigo <rodrigopba1@gmail.com>
2022-06-17 10:52:46 -0300

    evaluation data

--- MQTT-with-TLS/certs/client/subscriberA.sh
+++ MQTT-with-TLS/certs/client/subscriberA.sh
@@ -3,6 +3,8 @@
 #mosquitto_sub -p 8883 --cafile ../ca/ca.crt --cert client.crt --key client.key -h localhost -t temp_with_suburb
 #mosquitto_sub -p 8883 -u suba -P suba@123 --cafile ../ca/ca.crt --cert client.crt --key client.key -h localhost -t temp_with_suburb | xargs -d$'\n' -L1 bash -c 'date +"%T.%3N"' >> end_time_subA.txt
 mosquitto_sub -p 8883 -u suba -P suba@123 --cafile ../ca/ca.crt --cert client.crt --key client.key -h localhost -t temp_with_suburb | xargs -d$"\n}\n}" -L1 bash -c 'date +"%T.%3N"' >> end_time_subA.txt
+#with debug
+#mosquitto_sub -p 8883 -u suba -P suba@123 --cafile ../ca/ca.crt --cert client.crt --key client.key -h localhost -t temp_with_suburb -d
 
 
 #now=$(date +"%T.%3N")
